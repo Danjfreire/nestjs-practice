7ccecf16d72ec67d2b21216e20b54dd69aa76b0b
Daniel Freire <danjfreire@gmail.com>
2022-09-22 19:41:10 -0300

    chore: added tests to tags controller and service

--- src/tags/tests/tags.controller.spec.ts
+++ src/tags/tests/tags.controller.spec.ts
@@ -4,17 +4,30 @@ import { TagsService } from '../tags.service';
 
 describe('TagsController', () => {
   let controller: TagsController;
+  let tagService : TagsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TagsController],
-      providers: [TagsService],
+      providers: [{ provide: TagsService, useValue: { findAll: jest.fn() } }],
     }).compile();
 
     controller = module.get<TagsController>(TagsController);
+    tagService = module.get<TagsService>(TagsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should find all tags', async () => {
+    const tags = ['tag1', 'tag2' , 'tag3'];
+
+    jest.spyOn(tagService, 'findAll').mockImplementation(async () => tags);
+
+    const res = await controller.findAll();
+
+    expect(tagService.findAll).toHaveBeenCalledTimes(1);
+    expect(res).toEqual({tags});
+  })
 });
--- src/tags/tests/tags.service.spec.ts
+++ src/tags/tests/tags.service.spec.ts
@@ -1,18 +1,44 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { async, distinct } from 'rxjs';
+import { Article } from '../../articles/schemas/article.schema';
 import { TagsService } from '../tags.service';
 
 describe('TagsService', () => {
   let service: TagsService;
+  let articleModel;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TagsService],
+      providers: [
+        TagsService,
+        { provide: getModelToken(Article.name), useValue: { find: jest.fn() } }
+      ],
     }).compile();
 
     service = module.get<TagsService>(TagsService);
+    articleModel = module.get(getModelToken(Article.name));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should find all tags', async () => {
+    const tags = ['tag1', 'tag2', 'tag3'];
+
+    const findQuery = { distinct : jest.fn(() => ({exec : async () => tags}))}
+
+    jest.spyOn(articleModel, 'find').mockImplementation(() => findQuery );
+
+    const res = await service.findAll();
+
+    expect(findQuery.distinct).toHaveBeenCalledWith('tagList');
+    expect(res).toEqual(tags);
+  })
+
+  it('should handle error trying to retrieve all tags', async () => {
+    test.todo
+  });
+
 });
